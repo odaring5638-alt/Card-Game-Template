abf90fcc7122bf9173f520c3bcd2419825f71e25
odaring5638-alt <odaring5638@student.pps.net>
2026-05-15 13:53:43 -0700

    save

--- Assets/Scripts/Draw_Pile.cs
+++ Assets/Scripts/Draw_Pile.cs
@@ -10,26 +10,27 @@ public class Draw_Pile : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Draw pile clicked, draws remaining: " + draws_remaining);
-        if (GameManager.gm.player_deck.Count > 0 && draws_remaining > 0)
+        if (GameManager.gm.deck.Count > 0 && draws_remaining > 0)
+        
         {
             Draw();
             draws_remaining--;
         }
-        else if (draws_remaining == 0)
-        {
-            Debug.Log("No draws remaining!");
-        }
-        else
-        {
-            Debug.Log("No cards left in deck!");
-        }
+        // else if (draws_remaining == 0)
+        // {
+        //     Debug.Log("No draws remaining!");
+        // }
+        // else
+        // {
+        //     Debug.Log("No cards left in deck!");
+        // }
     }
 
     void Draw()
     {
         Card current_card = Instantiate(GameManager.gm.blank, Vector3.zero, Quaternion.identity);
-        current_card.data = GameManager.gm.player_deck[0];
-        GameManager.gm.player_deck.Remove(current_card.data);
+        current_card.data = GameManager.gm.deck[0];
+        GameManager.gm.deck.Remove(current_card.data);
         GameManager.gm.player_hand.Add(current_card.data);
         current_card.transform.SetParent(GameManager.gm.canvas.transform);
         Debug.Log("Card drawn: " + current_card.data.card_name);
